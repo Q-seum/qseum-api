ea9d0ca5fc72fd2bf576be80b8b6f8fe4c021d90
Molly Brown <molly.stra@gmail.com>
2018-06-25 12:39:23 -0400

    login controller

--- qseum-api/app/controllers/application_controller.rb
+++ qseum-api/app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::API
+    include ActionController::HttpAuthentication::Token::ControllerMethods
+    include ActionController::MimeResponds
+    before_action :verify_authentication
+    helper_method :current_user
+    protect_from_forgery with: :null_session
+    
+
+    def current_user    
+        @c_user ||= User.find_by(api_token: bearer_token)
+        return @c_user
+    end
+    
+    def bearer_token
+        pattern = /^Bearer /
+        header  = request.headers['Authorization']
+        header.gsub(pattern, '') if header && header.match(pattern)
+    end
+    
+    def verify_authentication
+        user = authenticate_with_http_token do |token, options|
+            User.find_by(api_token: token)
+        end
+            
+        unless user
+            render json: { error: "You do not have permission to access these resources" }, status: :unauthorized
+        end
+    end
+
 end
