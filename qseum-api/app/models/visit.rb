3d1aa7a55b5945eb83f353fc673277c10b28cd26
Molly Brown <molly.stra@gmail.com>
2018-06-28 13:55:18 -0400

    changing Date to Time

--- qseum-api/app/models/visit.rb
+++ qseum-api/app/models/visit.rb
@@ -14,7 +14,7 @@
 class Visit < ApplicationRecord
     belongs_to :user, foreign_key: :user_id
 
-    scope :todays_visits, ->{ where(date: Date.today).order('created_at DESC') }
+    scope :todays_visits, ->{ where(date: Time.now.in_time_zone("Eastern Time (US & Canada)")).order('created_at DESC') }
 
     validates :user_id, :account, :date, :visitors, presence: true
 end
