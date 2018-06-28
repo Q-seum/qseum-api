049744ecc9980af6b0a526f76bf1bd9de980c916
Molly Brown <molly.stra@gmail.com>
2018-06-28 12:36:12 -0400

    error tree change

--- qseum-api/app/controllers/api/v1/users_controller.rb
+++ qseum-api/app/controllers/api/v1/users_controller.rb
@@ -9,10 +9,10 @@ class Api::V1::UsersController < ApplicationController
             if @user.save
                 render "api/v1/users/show.json", status:201
             else 
-                render json: @user.errors, status: 401
+                render json: {json.errors @user.errors}, status: 401
             end
         else
-            render json: {error: "Not a valid account number"}, status: 401
+            render json: {json.errors error: "Not a valid account number"}, status: 401
         end
     end
 
@@ -23,7 +23,7 @@ class Api::V1::UsersController < ApplicationController
         elsif @user == current_user
             render "api/v1/users/profile.json", status:200
         else
-            format.json { render json: @user.errors, status: :unprocessable_entity }
+            render json: { json.errors @user.errors, status: :unprocessable_entity }
         end
     end
 
@@ -34,7 +34,7 @@ class Api::V1::UsersController < ApplicationController
                 format.json { head :no_content }
             end
         else
-            format.json { render json: @user.errors, status: :unprocessable_entity }
+            render json: { json.errors @user.errors, status: :unprocessable_entity }
         end
     end
 
