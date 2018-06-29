30c16831bbb30757001fceb2bae2eb1201ab0e27
Jacobfh <jfh708@gmail.com>
2018-06-29 14:38:15 -0400

    mailer working, still need to adjust config in order to deploy

--- qseum-api/config/environments/development.rb
+++ qseum-api/config/environments/development.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
   host = 'https://q-seum.firebaseapp.com/'
-  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'https://q-seum.firebaseapp.com/' }
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
