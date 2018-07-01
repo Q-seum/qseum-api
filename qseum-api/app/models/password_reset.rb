1a09633bca59991ae5c59063af80736a83cd2f64
Molly Brown <molly.stra@gmail.com>
2018-07-01 09:41:40 -0400

    new_token param name

--- qseum-api/app/models/password_reset.rb
+++ qseum-api/app/models/password_reset.rb
@@ -15,11 +15,11 @@ class PasswordReset < ApplicationRecord
     belongs_to :user, foreign_key: :user_id
     
     
-    has_secure_token :api_token
+    has_secure_token :new_token
 
     def create_reset_digest
         self.reset_token = User.new_token
         update_attribute(:reset_digest, User.digest(reset_token))
     end
-    
+
 end
