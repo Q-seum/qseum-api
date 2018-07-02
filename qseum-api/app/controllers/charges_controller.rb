a94c24073bba616a7962fd4e38d659197a3bc597
Molly Brown <molly.stra@gmail.com>
2018-07-02 16:45:56 -0400

    testing stripe

--- qseum-api/app/controllers/charges_controller.rb
+++ qseum-api/app/controllers/charges_controller.rb
@@ -9,13 +9,13 @@ class ChargesController < ApplicationController
         @amount = params[:amount]
 
     
-        customer = Stripe::Customer.create(
-            :email => params[:stripeEmail],
-            :source  => params[:stripeToken]
-        )
+        # customer = Stripe::Customer.create(
+        #     :email => params[:stripeEmail],
+        #     :source  => params[:stripeToken]
+        # )
     
         charge = Stripe::Charge.create(
-            :customer    => customer.id,
+            :source  => params[:stripeToken]
             :amount      => @amount,
             :description => 'Rails Stripe customer',
             :currency    => 'usd'
