8152060851afcc55f1329f6c7284b4bef886cd39
Molly Brown <molly.stra@gmail.com>
2018-07-02 12:24:18 -0400

    domain env variable

--- qseum-api/config/environment.rb
+++ qseum-api/config/environment.rb
@@ -8,7 +8,7 @@ end
 ActionMailer::Base.smtp_settings = {
     :user_name => ENV['sendgrid_username'],
     :password => ENV['sendgrid_password'],
-    :domain => 'herokuapp.com',
+    :domain => ENV['my_domain'],
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
