728db05907c1baa71e2972f9b5625c08941741dd
Molly Brown <molly.stra@gmail.com>
2018-07-03 17:48:58 -0400

    email correction in mailer logic

--- qseum-api/app/mailers/ticket_mailer.rb
+++ qseum-api/app/mailers/ticket_mailer.rb
@@ -3,6 +3,10 @@ class TicketMailer < ApplicationMailer
     def send_ticket(ticket)
         @ticket = ticket
         @qr = RQRCode::QRCode.new("https://q-seum.firebaseapp.com/api/v1/tickets/#{@ticket.id}")
-        mail to: @ticket.recip_email, subject: "Q-seum Admission Ticket"
+        if @ticket.recip_email
+            mail to: @ticket.recip_email, subject: "Q-seum Admission Ticket"
+        else
+            mail to: @ticket.buyer_email, subject: "Q-seum Admission Ticket"
+        end
     end 
 end
