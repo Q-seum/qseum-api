24cf3b025f948334d39dadc97886614376616d92
Jacobfh <jfh708@gmail.com>
2018-07-03 11:37:15 -0400

    changed buyer email to recipient email in ticketmailer

--- qseum-api/app/mailers/ticket_mailer.rb
+++ qseum-api/app/mailers/ticket_mailer.rb
@@ -2,6 +2,6 @@ class TicketMailer < ApplicationMailer
 
     def send_ticket(ticket)
         @ticket = ticket
-        mail to: @ticket.buyer_email, subject: "Q-seum Admission Ticket"
+        mail to: @ticket.recip_email, subject: "Q-seum Admission Ticket"
     end 
 end
