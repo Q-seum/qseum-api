cceb6ebfb6f28ffd1898dfcab42a760f59f6072e
Jacobfh <jfh708@gmail.com>
2018-07-05 15:33:07 -0400

    converted qrcode to png date image

--- qseum-api/app/mailers/ticket_mailer.rb
+++ qseum-api/app/mailers/ticket_mailer.rb
@@ -1,11 +1,13 @@
 require 'rqrcode'
+require 'chunky_png'
+require 'rqrcode_png'
 
 class TicketMailer < ApplicationMailer
 
     def send_ticket(ticket)
         @ticket = ticket
-        @qr = RQRCode::QRCode.new("https://q-seum.firebaseapp.com/api/v1/tickets/#{@ticket.id}")
-        # attachments['QR Code'] = File.read("/app/views/ticket_mailer/send_ticket.html.erb")
+        @qr = RQRCode::QRCode.new("https://q-seum.firebaseapp.com/api/v1/tickets/#{@ticket.id}").to_img.resize(200,200).to_data_url
+        # attachments['QRCode'] = File.read()
 
         if @ticket.recip_email
             mail to: @ticket.recip_email, subject: "Q-seum Admission Ticket"
