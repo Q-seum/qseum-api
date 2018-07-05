ad1f106ebe526bb2ad0597957864a4ee6a1ff7df
Molly Brown <molly.stra@gmail.com>
2018-07-05 11:55:51 -0400

    redeemed conditional

--- qseum-api/app/controllers/api/v1/tickets_controller.rb
+++ qseum-api/app/controllers/api/v1/tickets_controller.rb
@@ -4,6 +4,10 @@ class Api::V1::TicketsController < ApplicationController
 
   def show
     @ticket = Ticket.find_by(params[:id])
+    if !@ticket.redeemed
+      render "api/v1/tickets/show.json", status:200
+    else
+      render json: { error: "This ticket has already been used." }
   end
 
   private
