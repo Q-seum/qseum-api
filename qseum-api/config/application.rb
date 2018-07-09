711a506afd56583215d20e2175d09b414bad09ed
Molly Brown <molly.stra@gmail.com>
2018-07-09 12:01:04 -0400

    redis ans sidekiq

--- qseum-api/config/application.rb
+++ qseum-api/config/application.rb
@@ -22,7 +22,7 @@ module QseumApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.active_job.queue_adapter = :sidekiq
     config.time_zone = 'Eastern Time (US & Canada)'
     # config.active_record.default_timezone = :local
     
